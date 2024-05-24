72a127c0ba687abd6100f2c41e255736ac0e086e
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-05-24 08:43:54 -0400

    Update twoSums.ts
    LeetCode Problem 1: Two Sums with O(n)

--- typescript/twoSums.ts
+++ typescript/twoSums.ts
@@ -1,3 +1,4 @@
+// Brute-Force Approach with O(n^2) 
 function twoSum(nums: number[], target: number): number[] {
     // save the length of the array
     let len: number = nums.length;
@@ -10,4 +11,21 @@ function twoSum(nums: number[], target: number): number[] {
     }
     // return an empty array if two numbers don't equal the target
     return [];
+};
+
+// Hash Map Approach with O(n)
+function twoSum(nums: number[], target: number): number[] {
+    // initialize length of received array
+    let len: number = nums.length;
+    // initialize a new map to hash
+    let numMap = new Map();
+    // iterate over the received array
+    for (let i: number = 0; i < len; i++) {
+        // get the difference between the target num and current iteration
+        let diff: number = target - nums[i];
+        // if the hsh map has the difference, return the indices of the values
+        if (numMap.has(diff)) return [numMap.get(diff), i];
+        // add the current iteration to the hash map
+        numMap.set(nums[i], i)
+    }
 };
