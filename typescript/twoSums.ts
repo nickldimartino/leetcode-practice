3b385423392db5aff6e7525adaa0a566f0e16c75
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-05-26 16:00:41 -0400

    Update twoSums.ts

--- typescript/twoSums.ts
+++ typescript/twoSums.ts
@@ -23,7 +23,7 @@ function twoSum(nums: number[], target: number): number[] {
     for (let i: number = 0; i < len; i++) {
         // get the difference between the target num and current iteration
         let diff: number = target - nums[i];
-        // if the hsh map has the difference, return the indices of the values
+        // if the hash map has the difference, return the indices of the values
         if (numMap.has(diff)) return [numMap.get(diff), i];
         // add the current iteration to the hash map
         numMap.set(nums[i], i)
