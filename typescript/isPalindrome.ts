7134515ffdaad92e0d2aecea82dc16494917973a
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-06-12 07:02:56 -0400

    Update isPalindrome.ts

--- typescript/isPalindrome.ts
+++ typescript/isPalindrome.ts
@@ -2,3 +2,6 @@ function isPalindrome(x: number): boolean {
     // return if the string type of the received number is equal to it's reverse form
     return x.toString() === x.toString().split("").reverse().join("");
 };
+
+// rewritten as one-liner
+const isPalindrome = (x: number) => x.toString() === x.toString().split("").reverse().join("");
