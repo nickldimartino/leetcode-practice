8f7dfa1674fff8a09d79ba3a4b7262108fd163bd
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-06-12 07:01:06 -0400

    Update twoSums.ts

--- typescript/twoSums.ts
+++ typescript/twoSums.ts
@@ -2,6 +2,7 @@
 function twoSum(nums: number[], target: number): number[] {
     // save the length of the array
     let len: number = nums.length;
+    
     // iterate through the array 
     for (let i: number = 0; i < len; i++) {
         for (let j: number = i+1; j < len; j++) {
@@ -9,23 +10,30 @@ function twoSum(nums: number[], target: number): number[] {
             if (nums[i] + nums[j] === target) return [i, j];
         }
     }
+    
     // return an empty array if two numbers don't equal the target
     return [];
 };
 
 // Hash Map Approach with O(n)
 function twoSum(nums: number[], target: number): number[] {
-    // initialize length of received array
-    let len: number = nums.length;
-    // initialize a new map to hash
+    // initalized variables
+    const len: number = nums.length;
     let numMap = new Map();
-    // iterate over the received array
-    for (let i: number = 0; i < len; i++) {
-        // get the difference between the target num and current iteration
+    let i: number = 0;
+
+    // iterate over recieved aray
+    for (i; i < len; i++) {
+        // compute difference
         let diff: number = target - nums[i];
-        // if the hash map has the difference, return the indices of the values
+
+        // if the map has the difference equivalent, return the two indices
         if (numMap.has(diff)) return [numMap.get(diff), i];
-        // add the current iteration to the hash map
+
+        // add the current iteration to the map
         numMap.set(nums[i], i)
     }
+
+    // return empty array if no values were found
+    return [];
 };
