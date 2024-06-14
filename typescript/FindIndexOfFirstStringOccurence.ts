f1f707a2418abdc263dea12c1f771c11d05b420c
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-06-14 07:46:57 -0400

    Update FindIndexOfFirstStringOccurence.ts

--- typescript/FindIndexOfFirstStringOccurence.ts
+++ typescript/FindIndexOfFirstStringOccurence.ts
@@ -2,3 +2,6 @@ function strStr(haystack: string, needle: string): number {
     // return the index of needle if it's in haystack or -1 if not
     return haystack.includes(needle) ? haystack.indexOf(needle) : -1;
 };
+
+// One-liner
+const strStr = (haystack: string, needle: string): number => {return haystack.includes(needle) ? haystack.indexOf(needle) : -1};
